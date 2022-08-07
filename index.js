521e9f6737aafcf51cc22f41ae3981b757a942a9
Ankur Chaubey <ankurcha98@gmail.com>
2022-08-07 13:30:32 +0530

    adding multiple images

--- index.js
+++ index.js
@@ -30,20 +30,41 @@ app.post('/mypost', async(req, res) => {
     console.log(req.body);
     console.log(req.files);
 
-    let file = req.files.samplefile
+    let result;
+    imageArray = []
 
-    result =  await cloudinary.uploader.upload(file.tempFilePath, {
-       folder: 'users' 
-    });
+    // ^case -multi images
+
+    if (req.files) {
+        for (let index = 0; index < req.files.samplefile.length; index++) {
+            let result = await cloudinary.uploader.upload(req.files.samplefile[index].tempFilePath, {
+                folder: "users"
+            })
+            imageArray.push({
+                public_id: result.public_id,
+                secure_url: result.secure_url
+            })
+        }
+    }
+
+    // ^### use case for single image
+    // let file = req.files.samplefile
+    // result =  await cloudinary.uploader.upload(file.tempFilePath, {
+    //    folder: 'users' 
+    // });
 
     console.log(result);
 
     details = {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
-        result
+        result,
+        imageArray
     }
+    console.log(details);
+
     res.send(details);
+
 });
 
 app.get("/mygetform", (req, res) => {
